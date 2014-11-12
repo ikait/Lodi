98130aa4a9aeac0b6a1a592a6d54851f0c5085fa
Taishi Ikai <ikai@jxj.jp>
2014-11-12 16:27:04 +0900

    Initial Commit

---
+++ Lodi/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lodi
+//
+//  Created by Taishi Ikai on 2014/11/12.
+//  Copyright (c) 2014年 Taishi Ikai. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
